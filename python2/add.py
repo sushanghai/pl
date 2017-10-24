67e574050057dd3225642d81994fd67202126a72
sushanghai <sushanghai@qq.com>
2017-10-24 15:31:18 +0800

    v1024

---
+++ python2/add.py
@@ -0,0 +1,5 @@
+num1 =  raw_input('input a number:')
+num2 =  raw_input('input a number2:')
+
+sum = int(num1)+int(num2)
+print "num1+num2 is:%s"% sum
---
+++ python2/ifelse.py
@@ -0,0 +1,5 @@
+name = raw_input('please input your name:')
+if name=='shanghai':
+    print "hello %s" % name
+else:
+    print 'alawy'
