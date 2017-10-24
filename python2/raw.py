993203194b6166be9f537007ef19133f7ceb1c95
sushanghai <sushanghai@qq.com>
2017-10-24 10:57:57 +0800

    v3

---
+++ python2/raw.py
@@ -0,0 +1,16 @@
+#coding=utf-8
+#获取输入的用户名
+#name = raw_input("please enter your name:")
+#print it
+#print name
+
+print 'hello world'
+print "hello reboot"
+print "I'm happy"
+print 'I"m happy'
+
+print '='*30
+name = 'shanghai'
+type = 'ops'
+
+print 'hello I am '+name+',and I love ' + type
