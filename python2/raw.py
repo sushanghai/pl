c43ecfd184539337cd4c305565eefaae0ec0e597
sushanghai <sushanghai@qq.com>
2017-10-25 13:47:07 +0800

    del2

--- python2/raw.py
+++ python2/raw.py
@@ -4,18 +4,15 @@
 #print it
 #print name
 
-print 'hello world'
-print "hello reboot"
-print "I'm happy"
-print 'I"m happy'
+#print 'hello world'
+#print "hello reboot"
+#print "I'm happy"
+#print 'I"m happy'
 
 print '='*30
 name = 'shanghai'
 type = 'ops'
-<<<<<<< HEAD
 #连接符号+
 print 'hello I am '+name+',and I love ' + type
-=======
 
 print 'hello I am '+name+',and I love ' + type
->>>>>>> a82fd60fcc3880638c0400d0a57fcee3aeaad01a
