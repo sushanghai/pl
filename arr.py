4eddcd84ec8b28c191b0dbe2750c3bd49fd53cc3
sushanghai <sushanghai@qq.com>
2017-10-30 18:36:26 +0800

    arr

---
+++ arr.py
@@ -0,0 +1,16 @@
+arr = [1,2,3,'hello',[1,3,4,[12,23,1,'wrong']]]
+
+a= arr[0]
+b=arr[1]
+c= arr[4]
+d=arr[-1]
+e=arr[-2]
+f= arr[-3]
+print a
+print b
+print c
+print d
+print e
+print f
+for i in arr:
+    print i
