11042253551c49e99973cf1b262ed1a40bbcb78f
sushanghai <sushanghai@qq.com>
2017-12-05 17:13:07 +0800

    flask

---
+++ python2/fileutil.py
@@ -0,0 +1,24 @@
+#user_pwd
+file_dict ={}
+
+#dirct ==> file
+def read_file():
+    with open('user.txt') as f:
+         for line in f.read().split('\n'):
+	     if line:
+	         temp = line.split(':')
+                 file_dict[temp[0]] = temp[1]
+#	 return file_dict.items()
+
+#print read_file()
+
+
+#file ==> dict
+
+def write_file():
+     file_arr = []
+     for user,pwd in file_dict.items():
+         file_arr.append('%s:%s'%(user,pwd))
+
+     with open('user.txt','w') as f:
+         f.write('\n'.join(file_arr))
--- python2/flask_web.py
+++ python2/flask_web.py
@@ -2,51 +2,32 @@
 #!/usr/bin/python27
 
 import getMem2
-from flask import Flask,request,render_template
+import fileutil 
+from flask import Flask,request,render_template,redirect
 app = Flask(__name__)
 
+fileutil.read_file()
 
 
 @app.route('/')
 def index():
     return render_template('login.html')
-    #return render_template('index.html')
-    #return render_template(getMem2.getMem())
-    # print getMem2.getMem()
-    # return getMem2.getMem()
-#    return 'hello world'
-def check_login(user,pwd):
-    with open('user.txt') as f:
-         user_line = f.read().split('\n')
-         user_list = f.readlines()
-	 user_pwd_list = []
-	 user_dict = {}
-	 for i in user_list:
-	     temp = i.strip()
-	     user_pwd_list.append(temp)
-	 for j in user_pwd_list:
-	     line = j.split(':')
-	     user_dict[line[0]]=line[1]
-	 #print user_dict.items()
-	 temp_list=user_dict.items()
-         #print temp_list
-	     
-	 user_pwd = '%s:%s'%(user,pwd)
-	 #print user_pwd
-	 #print user_line
-	 if user_pwd in user_line:
-	     if user_pwd == '%s:%s'%('admin','pwd'):
-	         return 'heloo' 
-	     else:
-	         return 'erro'
-	 else:
-	     return 'erro'
-@app.route('/login')
-def login_html():
+
+@app.route('/loginaction')
+def login():
     user = request.args.get('user')
     pwd = request.args.get('pwd')
+    erro_msg = ''
+    if user and pwd:
+        if user == 'admin' and  pwd == 'pwd':
+	     return redirect('/list')
+	else:
+	     return 'wrong user or password!'
+    return render_template('login.html',erro_msg=erro_msg)
 
-    return check_login(user,pwd)
+@app.route('/list')
+def userlist():
+     return render_template('admin.html',userlist=fileutil.file_dict.items())
 
 
 
