21225b4f3db0a84b7ecbe3adacc1ef68fadf5320
sushanghai <sushanghai@qq.com>
2017-12-06 16:09:56 +0800

    add_update

--- python2/flask_web.py
+++ python2/flask_web.py
@@ -30,6 +30,35 @@ def userlist():
      return render_template('admin.html',userlist=fileutil.file_dict.items())
 
 
+@app.route('/del')
+def del_user():
+     user = request.args.get('user')
+     fileutil.file_dict.pop(user)
+     fileutil.write_file()
+     return redirect('/list')
 
+@app.route('/adduser')
+def adduser():
+     user = request.args.get('user')
+     pwd = request.args.get('pwd')
+     if user in fileutil.file_dict:
+         return redirect('/list')
+     else:
+         fileutil.file_dict[user]=pwd
+         fileutil.write_file()
+         return redirect('/list')
+
+@app.route('/update')
+def updae_user():
+    user = request.args.get('user')
+    pwd = fileutil.file_dict.get(user)
+    return render_template('update.html',user=user,pwd=pwd)
+@app.route('/updateaction')
+def updateaciton():
+    user = request.args.get('user')
+    new_pwd = request.args.get('new_pwd')
+    fileutil.file_dict[user] = new_pwd
+    fileutil.write_file()
+    return redirect('/list')
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=9092,debug=True)
